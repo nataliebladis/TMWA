5c561edb3fe59906ea6dc1aa2354294af2238f7e
Natalie Bladis <62963585+nataliebladis@users.noreply.github.com>
2023-10-04 15:06:56 -0700

    Update max_profit_accounts.sql
    add in apn

--- max_profit_accounts.sql
+++ max_profit_accounts.sql
@@ -14,3 +14,13 @@ FROM loadres.bill_summary
 GROUP BY customernumber
 ORDER BY total_profit_month DESC
 FETCH FIRST 10 ROWS ONLY;
+
+
+
+SELECT * 
+FROM enquesta.cihistorymaster;
+
+SELECT aprem_no, aprem_cycle, aprem_init_service_date, 
+       change_date(aprem_init_service_date,'MMDDYYYY') AS aprem_init_date
+FROM cis.fciaprem;
+     
